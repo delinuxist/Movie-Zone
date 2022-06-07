0a1a8334784793e5915ac77a80ed7a35d5bd2c87
delinuxist <delinuxist@gmail.com>
2022-06-07 23:06:14 +0000

    made some few changes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const MovieCard = ({movie:{ imdbID, Year, Poster, Title, Type }}) => {
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
   return (
-    <div className=" group ">
-            
-              <img width={250} className=' h-[23rem] rounded-2xl' src={Poster === "N/A" ? "https://via.placeholder.com/400" : Poster} alt="" />
-           
-              
-            </div>
-  )
-}
+    <div className=" group bg-black/100 rounded-2xl relative shadow-2xl drop-shadow-2xl hover:scale-105 duration-700 ease-in-out">
+      <img
+        width={250}
+        className="group-hover:opacity-25 h-[23rem] rounded-2xl"
+        src={Poster === "N/A" ? "https://via.placeholder.com/400" : Poster}
+        alt=""
+      />
+      <div className=" absolute bg-[#212426] top-[18.1rem]  rounded-b-2xl  w-full group-hover:bg-transparent">
+        <div className="px-4 py-4">
+          <h1 className="  text-white font-extralight">{Type.toUpperCase()}</h1>
+          <h1 className=" text-[#f9d3b4] text-justify truncate">{Title}</h1>
+        </div>
+      </div>
+      <div className="absolute text-[#f9d3b4] font-thin invisible  top-1 p-2 group-hover:visible">
+        <span>{Year}</span>
+      </div>
+    </div>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
