ec6247962f61c897063d6e90dbf75e1417fb6d5c
delinuxist <delinuxist@gmail.com>
2022-06-07 23:06:33 +0000

    fixed errors and improved styling

--- src/App.js
+++ src/App.js
@@ -18,33 +18,37 @@ const App = () => {
 
   const searchMovies = async (title) => {
     const response = await axios.get(`${API_URL}&s=${title}`);
-
     setMovies(response.data.Search);
-
-    console.log(movies);
   };
 
   return (
-    <div className="p-4 bg-[#212426] h-full">
+    <div className="p-4 bg-[#212426] min-h-screen">
       <div className=" p-4 flex justify-center items-center flex-col">
         <h1 className=" text-3xl font-semibold bg-clip-text w-fit text-transparent tracking-wide bg-gradient-to-r from-[#f9d3b4] to-[#f9d3b400] ">
-          MoveZone
+          MovieZone
         </h1>
-        <div className=" w-full text-center relative ">
-          <input
-            type="text"
-            placeholder="Search for movies"
-            className=" py-2 px-3 bg-slate-600 m-5 rounded-3xl w-[71%] outline-none text-white"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+        <div className="pb-5 w-full text-center relative ">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              searchMovies(title);
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Search for movies"
+              className=" py-2 px-4 bg-slate-600 m-5 rounded-3xl w-[71%] outline-none text-white"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </form>
           <SearchIcon
             className=" w-6 absolute right-[17%] top-[1.8rem] text-orange-300 cursor-pointer"
             onClick={() => searchMovies(title)}
           />
         </div>
         <div className=" container ">
-          <div className=" grid md:grid-cols-2 lg:grid-cols-4 gap-4 justify-items-center">
+          <div className=" grid md:grid-cols-2 lg:grid-cols-4 gap-5 justify-items-center">
             {!movies ? (
               <h1 className=" text-white">No Movies Found</h1>
             ) : (
